9b52a13224b182ddfb691f45f0fb6290910c16ad
Bego96 <begic.ismar96@gmail.com>
2023-12-12 15:04:52 +0100

    Filtering by value finnished

--- force-app/main/default/lwc/project/project.js
+++ force-app/main/default/lwc/project/project.js
@@ -1,11 +1,13 @@
 import { LightningElement, wire, track, api } from 'lwc';
 import getProjectList from '@salesforce/apex/getProjects.getProjectList';
 import getProjectsByDay from '@salesforce/apex/getProjects.getProjectsByDay';
+import sortByValue from '@salesforce/apex/getProjects.sortByValue';
 
 export default class Project extends LightningElement {
     @api day;
+    @api valuesort;
     @track projects;
-
+    
     @wire(getProjectList)
     projectsList({ error, data }) {
         if (data) {
@@ -30,5 +32,16 @@ export default class Project extends LightningElement {
         }
     }
 
+    @wire(sortByValue, {valuesort: '$valuesort'})
+    projectsByValue({error, data}) {
+        if (data) {
+            this.projects = data;
 
+            console.log(data);
+            console.log(this.projects);
+        } else if (error) {
+            console.log(error);
+        }
+    }
+    
 }
